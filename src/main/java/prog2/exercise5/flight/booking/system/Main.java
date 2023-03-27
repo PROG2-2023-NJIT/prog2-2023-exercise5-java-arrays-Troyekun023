cd5c4b5a9d70cb30a3bd8e9751c01ef7eb6629f6
Troyekun023 <126432309+Troyekun023@users.noreply.github.com>
2023-03-27 17:01:56 +0800

    Update Main.java

--- src/main/java/prog2/exercise5/flight/booking/system/Main.java
+++ src/main/java/prog2/exercise5/flight/booking/system/Main.java
@@ -1,12 +1,27 @@
 package prog2.exercise5.flight.booking.system;
 
-public class Main 
-{
-    public static void main( String[] args )
-    {   
-        int size = 3;
-        FlightBooking fb = new FlightBooking(size);
-        fb.reserveTickets();
 
-        }
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args) {
+
+
+        Scanner GQK = new Scanner(System.in);
+////////////////////////////////////////////////////////////////////////////////////
+        System.out.println("Please enter how many tickets you want to book:");
+        int number  =GQK.nextInt();
+        FlightBooking FB2 = new FlightBooking(number);
+        FB2.reserveTickets(number);
+        FB2.displayTripDetails();
+        GQK.close();
+////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+        
+    }
     }
